f1fd67fe2fd79e225d471fcbc091c9e0d8ef397b
qlhan <qlhan@incarcloud.com>
2022-02-17 10:20:49 +0800

    0.4.0-SNAPSHOT advanced syntax

---
+++ src/test/java/com/sample/drools/KBase5Tests.java
@@ -0,0 +1,29 @@
+package com.sample.drools;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * KBase5Tests
+ *
+ * @author Aaric, created on 2022-02-17T10:03.
+ * @version 0.5.0-SNAPSHOT
+ */
+@Slf4j
+public class KBase5Tests {
+
+    @Test
+    public void testRule() {
+        KieServices services = KieServices.Factory.get();
+        KieContainer container = services.getKieClasspathContainer();
+        KieSession session = container.newKieSession();
+
+        int count = session.fireAllRules();
+        session.dispose();
+
+        log.info("fire {} rules", count);
+    }
+}
